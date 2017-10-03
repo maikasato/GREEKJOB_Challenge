71b53e6358e4ade24a8a94e2bdb7cdc4e077bf62
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-03 17:56:58 +0900

    java課題提出　条件分岐　if文　

---
+++ CampChallenge_java/CampChallenge_java/005.条件分岐/課題3.java
@@ -0,0 +1,27 @@
+
+
+public class 課題3 {
+
+	public static void main(String[] args)
+	{
+
+		int NUM = 1;
+
+
+		if(NUM == 1){
+
+			System.out.print("１です！");
+
+		} else if (NUM == 2) {
+
+			System.out.print("プログラミングキャンプ！");
+
+		} else {
+			System.out.print("その他です！");
+
+
+		}
+
+
+	}
+}
