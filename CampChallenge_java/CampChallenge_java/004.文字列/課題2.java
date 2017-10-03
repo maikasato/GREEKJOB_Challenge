f7e6eb376284ba1cd3efc1c04f932d498cd05ca8
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-03 17:35:13 +0900

    課題提出します　文字列

---
+++ CampChallenge_java/CampChallenge_java/004.文字列/課題2.java
@@ -0,0 +1,19 @@
+
+public class 課題2 {
+
+	public static void main(String[] args)
+	{
+
+
+		String character = "gear"; //
+
+		String kigo = "-";
+
+		String name = "groove";
+
+
+		System.out.print(character + kigo + name);
+
+	}
+}
+
