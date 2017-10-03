558644eadbb6e574b50d3eebb16fb4e89043ac1d
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-03 16:42:47 +0900

    Java　四則演算　課題提出します

---
+++ CampChallenge_java/CampChallenge_java/003.四則演算/課題1.java
@@ -0,0 +1,26 @@
+
+public class 課題1 {
+
+
+	final int BASE = 100;
+	int num = 10;
+
+	int tashizan = BASE + num;
+
+
+	int hikizan = BASE - num;
+
+
+	int kakezan = BASE * num;
+
+
+	int warizan = BASE / num;
+
+
+	int wattaamari = BASE % 3;
+
+
+
+
+
+}
