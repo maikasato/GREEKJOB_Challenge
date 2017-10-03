0bf02d961ebeba28a7abd2ffd47986089fd77b03
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-03 18:49:30 +0900

    java課題　条件分岐　swich文2　提出します

---
+++ CampChallenge_java/CampChallenge_java/005.条件分岐/課題5.java
@@ -0,0 +1,28 @@
+
+public class 課題5 {
+	public static void main(String[] args)
+	{
+
+		String Char =" 英語";
+
+
+
+		switch(Char) {
+
+
+		case "A":
+			System.out.print("英語");
+
+			break;
+
+		case "あ":
+			System.out.print("日本語");
+
+
+			break;
+		}
+
+	}
+}
+
+
