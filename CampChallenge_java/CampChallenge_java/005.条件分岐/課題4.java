cc0dd98165face84aaa90950655e9a87e0d449f7
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-03 18:44:34 +0900

    java課題提出　条件分岐swich

---
+++ CampChallenge_java/CampChallenge_java/005.条件分岐/課題4.java
@@ -0,0 +1,27 @@
+
+public class 課題4 {
+	public static void main(String[] args)
+	{
+
+		int num = 1;
+
+
+
+		switch(num) {
+
+
+		case 1:
+			System.out.print("one");
+
+			break;
+
+		case 2:
+			System.out.print("two");
+
+		default:
+			System.out.print("予想外");
+			break;
+		}
+
+	}
+}
