74fe01de08fa830dfbf29fd2da1fefe0c24e1572
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-05 16:19:33 +0900

    java　ループ処理課題for文3　提出します

---
+++ CampChallenge_java/CampChallenge_java/008.ループ処理/課題11.java
@@ -0,0 +1,19 @@
+
+public class 課題11 {
+
+
+	public static void main(String[] args)
+	{
+
+
+	int total = 1;
+
+	for (int i = 1; i <= 100; i++) {
+	    total = total + i;
+	}
+
+	System.out.print(total);
+
+}
+
+}
