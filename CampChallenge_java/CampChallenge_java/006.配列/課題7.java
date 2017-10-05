cd067c4d2e67248b2688714beb06c46971e7957c
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-05 15:07:00 +0900

    java配列　ArrayList　提出します

---
+++ CampChallenge_java/CampChallenge_java/006.配列/課題7.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class 課題7 {
+
+
+		public static void main(String[] args)
+		{
+
+
+
+			ArrayList<String> List = new ArrayList<String>();
+
+
+
+
+			System.out.print(List.get(2));
+
+			List.set(2, "33");
+
+			System.out.print(List.size());
+
+			List.remove(2); // data.remove("soeda");でも可
+
+
+		}
+
+
+
+
+
+
+
+	}
+
+
+
