ddd06e95b8ad4a7bf8ed10bac1f1b3455d884dad
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-05 15:20:43 +0900

    java　連想配列　課題提出

---
+++ CampChallenge_java/CampChallenge_java/007.連想配列/課題8.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class 課題8 {
+
+	public static void main(String[] args)
+	{
+
+
+	HashMap<String, String> prof =
+            new HashMap<String, String>();
+
+prof.put("1", "AAA");
+
+prof.put("hello", "world");
+
+prof.put("soeda", "33");
+
+prof.put("20", "20");
+
+
+	}
+
+}
