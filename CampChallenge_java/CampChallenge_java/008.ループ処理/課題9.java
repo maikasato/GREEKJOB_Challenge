8bbed0bbc78f138852f85cc4e5923f5ed08538a4
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-05 15:39:09 +0900

    java　ループ処理for文1　課題提出します

---
+++ CampChallenge_java/CampChallenge_java/008.ループ処理/課題9.java
@@ -0,0 +1,20 @@
+
+public class 課題9 {
+
+	public static void main(String[] args)
+	{
+		int multiplied= 8;
+
+
+
+		for (int i = 1; i <= 20; i++) {
+
+
+			multiplied = multiplied * i;
+		}
+
+		System.out.print(multiplied);
+
+	}
+
+}
