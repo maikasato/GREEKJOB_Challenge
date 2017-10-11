ff658fc2d718ac882bfd19e5c628af055086ae13
DESKTOP-4PJV271\user <incident.lecture@gmail.com>
2017-10-11 14:53:15 +0900

    java メソッド　課題提出します

---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/課題13.java
@@ -0,0 +1,23 @@
+
+
+public class 課題13 {
+
+	  public static void main(String[] args){
+		  for(int i=0; i<10; i++){
+
+	      sub();
+	   }
+
+	  }
+	   public static void sub(){
+
+
+	      System.out.println("私は佐藤といいます");
+	      System.out.println("生年月日は5月24日です");
+	      System.out.println("趣味は海外旅行です");
+	   }
+	}
+
+
+
+
